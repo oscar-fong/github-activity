88a3f224e6b7ffa6813247ec8343ee179e812592
Oscar Fong <oscarfongfuentes@gmail.com>
2025-11-03 04:57:41 -0700

    More event cases added (Issues, Discussions, Delete, etc.)

--- github-activity.php
+++ github-activity.php
@@ -10,18 +10,50 @@ $opts = [
 $context = stream_context_create($opts);
 $result = file_get_contents($url, false, $context);
 $userActivity = json_decode($result, true);
-$activitySummary = [];
 foreach ($userActivity as $activity) {
   $type = $activity['type'];
-  $repoName = $activity['repo']['name'];
-  if (isset($activitySummary[$type][$repoName])) {
-    $activitySummary[$type][$repoName]++;
-  } else {
-    $activitySummary[$type][$repoName] = 1;
+  $repo = $activity['repo']['name'];
+  switch ($type) {
+    case 'PushEvent':
+      if (isset($summary[$type][$repo])) {
+        $summary[$type][$repo]++;
+      } else {
+        $summary[$type][$repo] = 1;
+      }
+      break;
+    case 'CreateEvent':
+    case 'DeleteEvent':
+      $summary[$type][$repo] = $activity['payload']['ref_type'];
+      break;
+    case 'DiscussionEvent':
+      $summary[$type][$repo] = $activity['payload']['discussion']['title'];
+      break;
+    case 'IssuesEvent':
+      $summary[$type][$repo] = ['action' => $activity['payload']['action'], 'issue' => $activity['payload']['issue']['title']];
+      break;
   }
 }
-
-foreach ($activitySummary as $key => $activity) {
-  echo $key;
-  print_r($activity);
+foreach ($summary as $type => $activity) {
+  foreach ($activity as $repo => $content) {
+    switch ($type) {
+      case 'PushEvent':
+        echo '- Pushed ', $content, ' commits to ', $repo, PHP_EOL;
+        break;
+      case 'CreateEvent':
+        echo '- Created ', $content, ' ', $repo, PHP_EOL;
+        break;
+      case 'DeleteEvent':
+        echo '- Deleted ', $content, ' ', $repo, PHP_EOL;
+        break;
+      case 'DiscussionEvent':
+        echo "- Created discussion in $repo: \"$content\"", PHP_EOL;
+        break;
+      case 'IssuesEvent':
+        $action = $content['action'];
+        $action[0] = strtoupper($action[0]);
+        $issue = $content['issue'];
+        echo "- $action issue in $repo: \"$issue\"", PHP_EOL;
+        break;
+    }
+  }
 }
